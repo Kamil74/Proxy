bb8255f3b05b712e7a7fee18d5655e4ed81eff0e
Rafael Milewski <rafimilewski@hotmail.com>
2023-02-14 13:44:20 +0200

    Initial commit

---
+++ Proxy/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Proxy;
+
+YouTubeService youTubeService = new YouTubeService();
+
+
+
+ProxyYouTubeService proxyYouTubeService = new ProxyYouTubeService(youTubeService);
+proxyYouTubeService.GetVideo(10);
+proxyYouTubeService.GetVideo(10);
---
+++ Proxy/YouTubeService.cs
@@ -0,0 +1,11 @@
+﻿namespace Proxy;
+
+public class YouTubeService : IYouTubeService
+{
+    public byte[] GetVideo(int videoId)
+    {
+        Console.WriteLine($"YouTube service downloading video: {videoId}");
+        
+        return new byte[videoId];
+    }
+}
